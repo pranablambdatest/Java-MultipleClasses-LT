32e546ab08bfaefd0d1de08178cf12ff4f344060
pranaybachu <Pranayb@lambdatest.com>
2023-03-02 19:04:23 -0500

    Commit code

---
+++ src/main/java/DemoDesktopScript2.java
@@ -0,0 +1,58 @@
+import io.appium.java_client.AppiumDriver;
+import org.apache.commons.lang3.time.StopWatch;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DemoDesktopScript2 extends demoDesktop {
+    @Test
+    public void testMethod() {
+        // RemoteWebDriver driver = this.driver;
+        try {
+            driver.get("https://www.nike.com/");
+//            WebDriverWait wait = new WebDriverWait(driver, 30);
+//            WebElement firstItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div > div > div > ul > li:nth-child(1) > input")));
+//            WebElement secondItem = driver.findElement(By.cssSelector("body > div > div > div > ul > li:nth-child(2) > input"));
+//            WebElement thirdItem = driver.findElement(By.cssSelector("body > div > div > div > ul > li:nth-child(4) > input"));
+//            WebElement fifthElement = driver.findElement(By.cssSelector("body > div > div > div > ul > li:nth-child(5) > input"));
+//            firstItem.click();
+//            secondItem.click();
+//            thirdItem.click();
+//            fifthElement.click();
+//            driver.findElement(By.id("sampletodotext")).sendKeys("new item added");
+//            driver.findElement(By.id("addbutton")).click();
+            status = "passed";
+        } catch(Exception e) {
+            e.printStackTrace();
+            status = "failed";
+            driver.quit();
+        }
+    }
+    @AfterTest
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+}
